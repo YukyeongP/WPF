d37e8d64f0b295c1c72393ae8968e8d5ba1cfdb1
YukyeongP <75259098+YukyeongP@users.noreply.github.com>
2022-02-08 18:17:46 +0900

    Add files via upload

--- WPF5/Program.cs
+++ WPF5/Program.cs
@@ -1,20 +1,41 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace WPF5
 {
     class Program
     {
-        public string Path;
+        //private BlockingCollection<string> myBC;
+        //public BlockingCollection<string> ProgramBc { get { return myBC; } }
 
         static void Main(string[] args)
         {
-            var allDrives = DriveInfo.GetDrives();
-            var rootDir = allDrives[0].RootDirectory.ToString();
+            var allDrives = DriveInfo.GetDrives(); 
+            ConcurrentBag<string> bag = new ConcurrentBag<string>();
 
-            var a = new SearchClass(rootDir);
-            a.StartSearch(rootDir, "*.*");
-            a.WriteFile(rootDir);
+            foreach (DriveInfo drive in allDrives)
+            { 
+                var thread = new Thread(new ParameterizedThreadStart(Working));
+                thread.Start(drive);
+                //bag.Add();
+            }
+
+        }
+
+        public static void Working(object drive)
+        {
+            var drives = drive as DriveInfo;
+            var rootDir = drives.RootDirectory.ToString();
+            var searchClass = new SearchClass(rootDir);
+
+            searchClass.StartSearch(rootDir, "*.dll|*.exe");
+            //searchClass.WriteFile(rootDir);
+
+            //////////
+            var bag = searchClass.CollectFiles2();
         }
     }
 }
--- WPF5/SearchClass.cs
+++ WPF5/SearchClass.cs
@@ -1,66 +1,123 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace WPF5
 {
     class SearchClass
     {
-        public string Path;
-        public IEnumerable<string> dllFiles;
-        public IEnumerable<string> exeFiles;
+        public string Path { get; set; }
+        public IEnumerable<string> AllFiles;
+        public List<String> FileCollection { get; set; } = new List<string>();
+        public List<String> Extensions { get; set; } = new List<string>();
 
-        public SearchClass() { }
         public SearchClass(string path)
         {
             Path = path;
         }
 
-        public void StartSearch(string dir, string file)
+        private void InitExt(string ext)
         {
-            var allFiles = GetAllFiles(dir, file);
-            dllFiles = allFiles.Where(s => s.EndsWith(".dll"));
-            exeFiles = allFiles.Where(s => s.EndsWith(".exe"));
+            ext = ext.Replace("*", "").Replace(".","");
+
+            if (ext.Contains('|'))
+            {
+                Extensions = ext.Split('|').ToList();
+            }
+            else
+            {
+                Extensions.Add(ext); 
+            }
+        }
+
+        public void StartSearch(string dir, string ext)
+        {
+            InitExt(ext);
+            AllFiles = GetAllFiles(dir, "*.*");
         }
 
         public void WriteFile(string dir)
         {
-            var dllSavePath = @"C:\Users\byk11\Desktop\" + dir[0] + "_dll.txt";
-            var exeSavePath = @"C:\Users\byk11\Desktop\" + dir[0] + "_exe.txt";
-
-            using (StreamWriter outputFile = new StreamWriter(dllSavePath))
+            var originPath = @"D:\Output\";
+            for (int i = 0; i < Extensions.Count; i++)
             {
-                foreach (var dllFile in dllFiles)
+                var savePath = originPath + dir[0] + "_" + Extensions[i] + ".txt";
+                using (StreamWriter outputFile = new StreamWriter(savePath))
                 {
-                    outputFile.WriteLine(dllFile);
+                    var ct = 0;
+                    foreach (var files in AllFiles.Where(s => s.EndsWith(Extensions[i])))
+                    {
+                        ct++;
+                        outputFile.WriteLine(files);
+                        if (ct == 1000) break;
+                    }
                 }
             }
+        }
 
-            using (StreamWriter outputFile = new StreamWriter(exeSavePath))
+        public ConcurrentBag<string> CollectFiles2()
+        {
+            ConcurrentBag<string> cb = new ConcurrentBag<string>();
+
+            for (int i = 0; i < Extensions.Count; i++)
             {
-                foreach (var exeFile in exeFiles)
+                var savePath = @"D:\Output\ExeAndDllFiles3.txt";
+                using (StreamWriter outputFile = new StreamWriter(savePath))
                 {
-                    outputFile.WriteLine(exeFile);
+                    var ct = 0;
+                    foreach (var files in AllFiles.Where(s => s.EndsWith(Extensions[i])))
+                    {
+                        ct++;
+                        cb.Add(files);
+                        if (ct == 1000) break;
+                    }
                 }
             }
+            return cb;
+        }
+
+
+        public ConcurrentBag<string> CollectFiles()
+        {
+            object collectFilesLock = new object();
+            ConcurrentBag<string> cb = new ConcurrentBag<string>();
+            var ct=0;
+
+            lock (collectFilesLock)
+            {
+                var savePath = @"D:\Output\ExeAndDllFiles2.txt";
+                for (int i = 0; i < Extensions.Count; i++)
+                {
+                    foreach (var files in AllFiles.Where(s => s.EndsWith(".dll") || s.EndsWith(".exe")))
+                    {
+                        ct++;
+                        //Console.WriteLine(files);
+                        //FileCollection.Add(files);
+                        cb.Add(files);
+                        if (ct == 1000) break;
+                    }
+                }
+                File.WriteAllLines(savePath, FileCollection);
+            }
+            return cb;
         }
 
         private IEnumerable<String> GetAllFiles(string path, string searchPattern)
         {
             return Directory.EnumerateFiles(path, searchPattern).Union(
                 Directory.EnumerateDirectories(path).SelectMany(d =>
+            {
+                try
                 {
-                    try
-                    {
-                        return GetAllFiles(d, searchPattern);
-                    }
-                    catch (Exception e)
-                    {
-                        return Enumerable.Empty<String>();
-                    }
-                }));
+                    return GetAllFiles(d, searchPattern);
+                }
+                catch (Exception e)
+                {
+                    return Enumerable.Empty<String>();
+                }
+            }));
         }
     }
 }
